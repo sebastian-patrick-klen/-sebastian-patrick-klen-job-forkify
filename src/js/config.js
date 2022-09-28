20b8c26d390d18b7e02e3eb4e1b7a9199dd4c528
sebastian klen <sebastianklen.business@gmail.com>
2022-09-28 21:33:47 +0200

    add api key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '9afd9fab-047e-42af-b0ba-9a8271b0a8ab';
 export const MODAL_CLOSE_SEC = 2.5;
